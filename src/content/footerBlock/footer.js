94f4e85d2d2705a0a668874054dd05bed04b96af
kiam <kiam125@gmail.com>
2020-10-11 04:17:43 +0800

    Feature: add footer

--- src/content/footerBlock/footer.js
+++ src/content/footerBlock/footer.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { FooterBlock } from './style';
+import { FooterBlock, Block, FooterListBlock, FooterListItem } from './style';
+import buy from '../../images/pic/buy.jpg';
+import help from '../../images/pic/help.jpg';
+import like from '../../images/pic/like.jpg';
+import right from '../../images/pic/right.jpg';
 
 export default class Footer extends Component {
 
@@ -7,8 +11,42 @@ export default class Footer extends Component {
     render() {
         return (
             <FooterBlock>
-                
-                
+                <Block>
+                    {/* <div style={{backgroundColor:"orange", height:"100px", width:"100%", display:"inline-block"}}></div>
+                    <div style={{backgroundColor:"red", height:"100px", width:"100%"}}></div> */}
+                    <FooterListBlock>
+                        <FooterListItem>
+                            <img src={right} alt="" style={{height:"25px", width:"25px"}}></img>
+                            <font color="white" size="5">正品保證</font>
+                        </FooterListItem>
+                        <FooterListItem>
+                            <img src={like} alt="" style={{height:"25px", width:"25px"}}></img>
+                            <font color="white" size="5">售後無憂</font>
+                        </FooterListItem>
+                        <FooterListItem>
+                            <img src={help} alt="" style={{height:"25px", width:"25px"}}></img>
+                            <font color="white" size="5">幫助中心</font>
+                        </FooterListItem>
+                        <FooterListItem>
+                            <img src={buy} alt="" style={{height:"25px", width:"25px"}}></img>
+                            <font color="white" size="5">省心購</font>
+                        </FooterListItem>
+                    </FooterListBlock>
+                    <FooterListBlock>
+                        <FooterListItem>
+                            <font color="white" size="5">購物指南</font>
+                        </FooterListItem>
+                        <FooterListItem>
+                            <font color="white" size="5">商家服務</font>
+                        </FooterListItem>
+                        <FooterListItem>
+                            <font color="white" size="5">商家服務</font>
+                        </FooterListItem>
+                        <FooterListItem>
+                            <font color="white" size="5">商家服務</font>
+                        </FooterListItem>
+                    </FooterListBlock>
+                </Block>
             </FooterBlock>
         );
     }
--- src/content/footerBlock/style.js
+++ src/content/footerBlock/style.js
@@ -6,3 +6,23 @@ export const FooterBlock = styled.div`
     margin-top: 50px;
     background-color: black;
 `;
+
+export const Block = styled.div`
+    width: 1200px;
+    height: 100%;
+    margin: 0 auto;
+    display: flow-root;
+    margin-top: 50px;
+`;
+
+export const FooterListBlock = styled.div`
+    margin-top: 30px;
+    display: block;
+    height: 50px;
+`;
+
+export const FooterListItem = styled.div`
+    width: 25%;
+    height: 50px;
+    display: inline-block;
+`;
